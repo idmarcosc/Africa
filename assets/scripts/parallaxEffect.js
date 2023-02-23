1e3f55eb3fd7c887019e4d4d016a138874f345be
idmarcosc <92194475+idmarcosc@users.noreply.github.com>
2023-02-23 12:26:38 -0300

    efeito parallax

---
+++ assets/scripts/parallaxEffect.js
@@ -0,0 +1,18 @@
+function parallaxEffect() {
+    let ceu = document.getElementById('ceu');
+        let montanha = document.getElementById('montanha');
+        let nuvem = document.getElementById('nuvem');
+        let arvore = document.getElementById('arvore');
+        let text = document.getElementById('text');
+
+        window.addEventListener('scroll', function(){
+            let value = window.scrollY;
+            ceu.style.top = value / 10 + 'px';
+            nuvem.style.left = value * 0.20 + 'px';
+            montanha.style.top = value * 0.40 + 'px';
+            arvore.style.left = value * 0.10 + 'px';
+            text.style.marginLeft = value * 1 + 'px';
+            text.style.marginTop = value * 2 + 'px';
+        })
+
+}
