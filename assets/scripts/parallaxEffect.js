fb286dcee7c7401c0509e8ca735c0ccb0ad5152d
idmarcosc <92194475+idmarcosc@users.noreply.github.com>
2023-02-23 13:25:12 -0300

    parallax bug fix

--- assets/scripts/parallaxEffect.js
+++ assets/scripts/parallaxEffect.js
@@ -1,6 +1,7 @@
 function parallaxEffect() {
     let ceu = document.getElementById('ceu');
         let montanha = document.getElementById('montanha');
+        let sol = document.getElementById('sol');
         let nuvem = document.getElementById('nuvem');
         let arvore = document.getElementById('arvore');
         let text = document.getElementById('text');
@@ -8,6 +9,8 @@ function parallaxEffect() {
         window.addEventListener('scroll', function(){
             let value = window.scrollY;
             ceu.style.top = value / 10 + 'px';
+            sol.style.top = '-' + value * 0.10 + 'px';
+            sol.style.left = '-' + value * 0.25 + 'px';
             nuvem.style.left = value * 0.20 + 'px';
             montanha.style.top = value * 0.40 + 'px';
             arvore.style.left = value * 0.10 + 'px';
