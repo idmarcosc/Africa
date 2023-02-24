9c7a70fec076bdc1548e126768dd6ab539e725a7
idmarcosc <92194475+idmarcosc@users.noreply.github.com>
2023-02-24 11:19:43 -0300

    media query

---
+++ assets/scripts/menuMobile.js
@@ -0,0 +1,14 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.navBar');
+    let iconMenu = document.getElementById("icon-menu").innerHTML;
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+    if (iconMenu == 'menu') {
+        document.getElementById("icon-menu").innerHTML = "close";
+    } else {
+        document.getElementById("icon-menu").innerHTML = "menu";
+    }
+}
--- assets/scripts/parallaxEffect.js
+++ assets/scripts/parallaxEffect.js
@@ -1,21 +1,26 @@
 function parallaxEffect() {
-    let ceu = document.getElementById('ceu');
+    if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+        let ceu = document.getElementById('ceu');
         let montanha = document.getElementById('montanha');
         let sol = document.getElementById('sol');
         let nuvem = document.getElementById('nuvem');
         let arvore = document.getElementById('arvore');
         let text = document.getElementById('text');
+        let btn = document.getElementById('btn');
 
         window.addEventListener('scroll', function(){
             let value = window.scrollY;
             ceu.style.top = value / 10 + 'px';
             sol.style.top = '-' + value * 0.10 + 'px';
             sol.style.left = '-' + value * 0.25 + 'px';
-            nuvem.style.left = value * 0.20 + 'px';
+            nuvem.style.left = value * 0.60 + 'px';
             montanha.style.top = value * 0.40 + 'px';
             arvore.style.left = value * 0.10 + 'px';
             text.style.marginLeft = value * 1 + 'px';
             text.style.marginTop = value * 2 + 'px';
+            btn.style.marginTop = value * 2 + 'px';
+            btn.style.marginLeft = value * 1 + 'px';
         })
-
+    }
 }
